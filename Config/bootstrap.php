7f93a54850b2afd3c26682a78fce08ed6e1b0b92
Andrey Klimov <42044955+anklimsk@users.noreply.github.com>
2018-08-02 22:52:49 +0300

    Added files after creating the repository

---
+++ Config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is global util functions file of the application.
+ *
+ * CakeBasicFunctions: Basic global utilities for application.
+ * @copyright Copyright 2016, Andrey Klimov.
+ * @package plugin.Vendor
+ */
+
+App::import(
+    'Lib/Utility',
+    'CakeBasicFunctions.BasicFunctions',
+    ['file' => 'BasicFunctions.php']
+);
